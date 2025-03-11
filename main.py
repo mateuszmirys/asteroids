3b36f05e105020715805e906c52a8d868cd67535
redbrickland <mateusz@mirys.pl>
2025-03-11 21:31:20 +0100

    add clock to main.py, add circleshape.py

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ def main():
 	print(f"Screen height: {SCREEN_HEIGHT}")
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	
+	clock = pygame.time.Clock()
+	dt = 0
+
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
@@ -15,6 +17,8 @@ def main():
 
 		pygame.Surface.fill(screen, (0, 0, 0))
 		pygame.display.flip()
+		clock.tick(60)
+		dt = clock.tick() / 1000
 	
 if __name__ == "__main__":
 	main()
