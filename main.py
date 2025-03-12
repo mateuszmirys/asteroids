1611832b5274d3093f7bbc9031c566a76549d2e4
redbrickland <mateusz@mirys.pl>
2025-03-12 17:36:24 +0100

    add movement and rotation methods to player.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 from constants import *
 
 def main():
@@ -10,15 +11,21 @@ def main():
 	clock = pygame.time.Clock()
 	dt = 0
 
+	x = SCREEN_WIDTH / 2
+	y = SCREEN_HEIGHT / 2
+	player = Player(x, y)
+
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 
 		pygame.Surface.fill(screen, (0, 0, 0))
+		player.update(dt)
+		player.draw(screen)
 		pygame.display.flip()
 		clock.tick(60)
-		dt = clock.tick() / 1000
+		dt = clock.tick(60) / 1000
 	
 if __name__ == "__main__":
 	main()
